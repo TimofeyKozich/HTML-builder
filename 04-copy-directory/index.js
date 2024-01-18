be5b6c8cf4dc860b06667a781ed41cc55951187d
Timofey Kozich <kozich.timofey@gmail.com>
2024-01-18 11:45:32 +0300

    feat: complete task 04-copy-directory

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+const pathToFolder = path.join(__dirname, 'files');
+const pathToFolderCopy = path.join(__dirname, 'files-copy');
+
+fs.mkdir(pathToFolderCopy, { recursive: true }, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.readdir(pathToFolder, (err, filesInFolder) => {
+      if (err) {
+        console.log(err);
+      } else {
+        for (let file of filesInFolder) {
+        const pathToFile = path.join(pathToFolder, file);
+        const pathToFileCopy = path.join(pathToFolderCopy, file);
+
+        fs.copyFile(pathToFile, pathToFileCopy,  fs.constants.COPYFILE_FICLONE, (err) => {
+          if (err) {
+            console.log(err);
+          }
+        })
+        
+        }
+      }
+    })
+  }
+})
