8ea7e335606a3efa9f4294b8e5c9208df89d6663
Timofey Kozich <kozich.timofey@gmail.com>
2024-01-22 22:09:59 +0300

    fix: mistakes task 06 from cross-check guidelines

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -62,16 +62,27 @@ createBundle(pathToStyles, path.join(newDirPath, 'style.css'));
 
 async function createHTML() {
   try {
-    const articles = await fs.readFile(path.join(__dirname, 'components', 'articles.html'), {encoding: 'utf-8'});
-    const footer = await fs.readFile(path.join(__dirname, 'components', 'footer.html'), {encoding: 'utf-8'});
-    const header = await fs.readFile(path.join(__dirname, 'components', 'header.html'), {encoding: 'utf-8'});
     const template = await fs.readFile(path.join(__dirname, 'template.html'), {encoding: 'utf-8'});
 
-    const t1 = template.replace('{{articles}}', articles);
-    const t2 = t1.replace('{{header}}', header);
-    const t3 = t2.replace('{{footer}}', footer);
+    async function replaceComponent(temp) {
+      const firstPart = temp.slice(0, temp.indexOf('{{')).trim();
+      const secondPart = temp.slice(temp.indexOf('}}') + 2).trim();
 
-    await fs.writeFile(path.join(newDirPath, 'template.html'), t3);
+      const nameOfComponent = temp.slice(temp.indexOf('{{') + 2, temp.indexOf('}}'));
+      const component = await fs.readFile(path.join(__dirname, 'components', `${nameOfComponent}.html`), {encoding: 'utf-8'});
+
+      let newTemp = `${firstPart}\n${component.trim()}\n${secondPart}`;
+
+      if (newTemp.includes('{{')) {
+        newTemp = await replaceComponent(newTemp)
+      }
+
+      return newTemp;
+    }
+
+    replaceComponent(template);
+
+    await fs.writeFile(path.join(newDirPath, 'index.html'), await replaceComponent(template));
 
   } catch (err) {
     console.log(err);
